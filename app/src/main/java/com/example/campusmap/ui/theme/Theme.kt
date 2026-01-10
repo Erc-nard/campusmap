34bc8f09952a74e07da7ebbd5b9a37c219694da1
Ercnard <100018037+Erc-nard@users.noreply.github.com>
2026-01-10 18:47:33 +0900

    Shuttle (#6)
    * 반응형 해결(해상도마다 다르긴한데 아무튼 크게 차이안나서 ㄱㅊ)
    
    * 버스애니메이션하다가안됨
    
    * 글씨체변경
    
    ---------
    
    Co-authored-by: Woohyun Kang <30944771+KangWH@users.noreply.github.com>

--- app/src/main/java/com/example/campusmap/ui/theme/Theme.kt
+++ app/src/main/java/com/example/campusmap/ui/theme/Theme.kt
@@ -9,45 +9,49 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import com.example.campusmap.ui.theme.*
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    secondary = black,
+    tertiary = Pink80,
+    background = Color(0xFF1E1E1E),   // 다크 모드 배경
+    surface = Color(0xFF1E1E1E),      // 카드, 표면
+    onPrimary = white,           // primary 위 텍스트
+    onSecondary = white,
+    onBackground = white,        // 배경 위 텍스트
+    onSurface = white
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = white,
+    tertiary = Pink40,
+    background = Color(0xFFFFFFFF),   // 밝은 배경
+    surface = Color(0xFFFFFFFF),      // 카드, 표면
+    onPrimary = white,
+    onSecondary = black,
+    onBackground = black,       // 배경 위 텍스트
+    onSurface = black
 )
+
 
 @Composable
 fun CampusmapTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val context = LocalContext.current
+        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    } else {
+        if (darkTheme) DarkColorScheme else LightColorScheme
     }
 
     MaterialTheme(
